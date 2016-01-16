e6a6b95975147e2dbd982db6838a1f210f1dc894
zhanggangmin <490410638@qq.com>
2016-01-16 22:03:38 +0800

    搭建volley

--- app/src/main/java/com/zhanggangmin/volleydemo/GmApplication.java
+++ app/src/main/java/com/zhanggangmin/volleydemo/GmApplication.java
@@ -1,20 +1,14 @@
 package com.zhanggangmin.volleydemo;
 
 import android.app.Application;
-import android.text.TextUtils;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.Volley;
 
 /**
  * Created by zhanggangmin on 16/1/16.
  */
 public class GmApplication extends Application {
-    private static final String TAG = "GmDEBUG";
+
     private static GmApplication sInstance;
-    private RequestQueue mRequestQueue;
+
 
     public static synchronized GmApplication getInstance() {
         return sInstance;
@@ -28,43 +22,7 @@ public class GmApplication extends Application {
         sInstance = this;
     }
 
-    public RequestQueue getRequestQueue() {
-        // lazy initialize the request queue, the queue instance will be
-        // created when it is accessed for the first time
-        if (mRequestQueue == null) {
-            mRequestQueue = Volley.newRequestQueue(getApplicationContext());
-        }
-
-        return mRequestQueue;
-    }
-
-    public void addToRequestQueue(Request req, String tag) {
-        // set the default tag if tag is empty
-        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
-
-        VolleyLog.d("Adding request to queue: %s", req.getUrl());
-
-        getRequestQueue().add(req);
-    }
-
-    public void addToRequestQueue(Request req) {
-        // set the default tag if tag is empty
-        req.setTag(TAG);
-
-        getRequestQueue().add(req);
-    }
 
 
-    public void cancelPendingRequests(Object tag) {
-        if (mRequestQueue != null) {
-            mRequestQueue.cancelAll(tag);
-        }
 
-    }
-
-//    public void cacelRequestQueue() {
-//        for (Request<?> req : mRequestQueue) {
-//            req.cancel();
-//        }
-//    }
 }
--- app/src/main/java/com/zhanggangmin/volleydemo/Network.java
+++ app/src/main/java/com/zhanggangmin/volleydemo/Network.java
@@ -1,8 +1,65 @@
 package com.zhanggangmin.volleydemo;
 
+import android.content.Context;
+import android.text.TextUtils;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.Volley;
+
 /**
  * Created by zhanggangmin on 16/1/16.
  */
 public class Network {
+    private static final String TAG = "GmDEBUG";
+    private static Network ourInstance;
+    private RequestQueue mRequestQueue;
+    private Context context;
+
+    private Network(Context context) {
+        this.context = context;
+    }
+
+    public static Network getInstance(Context context) {
+        if (ourInstance == null) {
+            ourInstance = new Network(context);
+        }
+        return ourInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        // lazy initialize the request queue, the queue instance will be
+        // created when it is accessed for the first time
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(context.getApplicationContext());
+        }
+
+        return mRequestQueue;
+    }
+
+    public void addToRequestQueue(Request req, String tag) {
+        // set the default tag if tag is empty
+        req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
+
+        VolleyLog.d("Adding request to queue: %s", req.getUrl());
+
+        getRequestQueue().add(req);
+    }
+
+    public void addToRequestQueue(Request req) {
+        // set the default tag if tag is empty
+        req.setTag(TAG);
+
+        getRequestQueue().add(req);
+    }
+
+
+    public void cancelPendingRequests(Object tag) {
+        if (mRequestQueue != null) {
+            mRequestQueue.cancelAll(tag);
+        }
+
+    }
 
 }
